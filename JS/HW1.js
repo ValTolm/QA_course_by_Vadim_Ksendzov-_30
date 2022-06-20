db01ff4d2c9d09a11c6ff753883a0375a841ea96
ValeriaTol <vntolmacheva@gmail.com>
2022-06-20 23:10:26 +0400

    updated HW1 file

--- JS/HW1.js
+++ JS/HW1.js
@@ -87,14 +87,14 @@ checkAge2("17");
 checkAge2(18);
 checkAge2(61);
 
-//3*
+//3* 
 const checkAge3 = function(age) {
-    var age_1 = +age;
+    var age_1 = parseFloat(age);
     var age_2 = 18;
     var age_3 = 60;
-    if(!(typeof(age_1) == "number")){
-        console.error('Not a number!');
-    } else 
+    // if(!(typeof(age_1) == "number")){
+    //     console.error('Not a number!');
+    // } else 
     if(age_1 < age_2){
         console.log("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
     } else if(age_1 >= age_2 & age_1 < age_3) {
@@ -102,12 +102,106 @@ const checkAge3 = function(age) {
     } else if( age_1 > age_3){
         console.log("Keep calm and look Culture channel");
     } else {
-        console.log("Technical work");
+        console.error('Not a number!');
     }
 }
-checkAge3("17");
+checkAge3("ааа");
 checkAge3(18);
 checkAge3(61);
+checkAge3("");
+checkAge3(" ");
+checkAge3(0);
+
+//3* вариант 2
+const checkAge32 = function(age) {
+    var age_1 = age;
+    var age_2 = 18;
+    var age_3 = 60;
+    function checkAge(age_1){
+        if(age_1 < age_2){
+            console.log("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
+        } else if(age_1 >= age_2 & age_1 < age_3) {
+            console.log("Welcome  !");
+        } else if( age_1 > age_3){
+            console.log("Keep calm and look Culture channel");
+        }
+    }
+    if(typeof(age_1) == "number"){
+        checkAge(age_1)
+    } else {
+        var changeAgeType = parseFloat(age_1)
+        if(Number(changeAgeType)){
+            checkAge(changeAgeType);
+        } else {
+            console.error('Not a number!');
+        }
+    }
+}
+checkAge32("ааа");
+checkAge32("181");
+checkAge32(61);
+checkAge32("");
+checkAge32(" ");
+checkAge32(0);
+
+//3* 3 вариант
+const checkAge33 = function(age) {
+    var age_1 = parseFloat(age);
+    var age_2 = 18;
+    var age_3 = 60;
+    function checkAge(age_1){
+        if(age_1 < age_2){
+            console.log("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
+        } else if(age_1 >= age_2 & age_1 < age_3) {
+            console.log("Welcome  !");
+        } else if( age_1 > age_3){
+            console.log("Keep calm and look Culture channel");
+        }
+    }
+
+    if(isNaN(age_1)) {
+        console.error('Not a number!')        
+    } else {
+        checkAge(age_1)
+    }
+}
+checkAge33("ааа");
+checkAge33("181");
+checkAge33(61);
+checkAge33("");
+checkAge33(" ");
+checkAge33(0);
+
+//3* вариант от ментора if(!age || isNaN(age)) !!! не работает
+const checkAge34 = function(age) {
+    var age_1 = age;
+    var age_2 = 18;
+    var age_3 = 60;
+    function checkAge(age_1){
+        if(age_1 < age_2){
+            console.log("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
+        } else if(age_1 >= age_2 & age_1 < age_3) {
+            console.log("Welcome  !");
+        } else if( age_1 > age_3){
+            console.log("Keep calm and look Culture channel");
+        }
+    }
+
+    if(!age_1 || isNaN(age_1)) {
+        console.error('Not a number!')        
+    } else {
+        checkAge(age_1)
+    }
+}
+checkAge34("ааа");
+checkAge34("181");
+checkAge34(61);
+checkAge34("");
+checkAge34(" ");
+checkAge34(0);
+
+//3* вариант без преобразования заранее
+
 
 //4* см. отдельный файл
 var age = prompt("Введите свой возраст:");
